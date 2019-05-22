3c27e13aa09a178182513a4da0d4e4e0f0b4698b
ifaliefya <aliefya.fadhila@gmail.com>
2019-05-22 14:16:52 +0700

    usaha mengembalikan

--- GazethruApps/AdminInfoEdit.Designer.cs
+++ GazethruApps/AdminInfoEdit.Designer.cs
@@ -94,6 +94,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(69, 146);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(295, 236);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
