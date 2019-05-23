a164d3551b508a4c5b27074521af03322a93bc7c
ifaliefya <aliefya.fadhila@gmail.com>
2019-05-24 00:09:03 +0700

    on progress fix admin

---
+++ GazethruApps/AdminAwal.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GazethruApps
+{
+    partial class AdminAwal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminAwal));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_Slideshow = new System.Windows.Forms.Button();
+            this.btn_Tentang = new System.Windows.Forms.Button();
+            this.btn_Prestasi = new System.Windows.Forms.Button();
+            this.btn_Kegiatan = new System.Windows.Forms.Button();
+            this.btn_Peta = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btn_Peta);
+            this.panel1.Controls.Add(this.btn_Kegiatan);
+            this.panel1.Controls.Add(this.btn_Prestasi);
+            this.panel1.Controls.Add(this.btn_Tentang);
+            this.panel1.Controls.Add(this.btn_Slideshow);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 839);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_Slideshow
+            // 
+            this.btn_Slideshow.FlatAppearance.BorderSize = 0;
+            this.btn_Slideshow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Slideshow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Slideshow.Image = ((System.Drawing.Image)(resources.GetObject("btn_Slideshow.Image")));
+            this.btn_Slideshow.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Slideshow.Location = new System.Drawing.Point(24, 179);
+            this.btn_Slideshow.Name = "btn_Slideshow";
+            this.btn_Slideshow.Size = new System.Drawing.Size(210, 60);
+            this.btn_Slideshow.TabIndex = 0;
+            this.btn_Slideshow.Text = "   Slideshow";
+            this.btn_Slideshow.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Slideshow.UseVisualStyleBackColor = true;
+            // 
+            // btn_Tentang
+            // 
+            this.btn_Tentang.FlatAppearance.BorderSize = 0;
+            this.btn_Tentang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Tentang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Tentang.Image = ((System.Drawing.Image)(resources.GetObject("btn_Tentang.Image")));
+            this.btn_Tentang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Tentang.Location = new System.Drawing.Point(24, 245);
+            this.btn_Tentang.Name = "btn_Tentang";
+            this.btn_Tentang.Size = new System.Drawing.Size(210, 60);
+            this.btn_Tentang.TabIndex = 0;
+            this.btn_Tentang.Text = "   Tentang DTETI";
+            this.btn_Tentang.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Tentang.UseVisualStyleBackColor = true;
+            // 
+            // btn_Prestasi
+            // 
+            this.btn_Prestasi.FlatAppearance.BorderSize = 0;
+            this.btn_Prestasi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Prestasi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Prestasi.Image = ((System.Drawing.Image)(resources.GetObject("btn_Prestasi.Image")));
+            this.btn_Prestasi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Prestasi.Location = new System.Drawing.Point(24, 311);
+            this.btn_Prestasi.Name = "btn_Prestasi";
+            this.btn_Prestasi.Size = new System.Drawing.Size(210, 60);
+            this.btn_Prestasi.TabIndex = 0;
+            this.btn_Prestasi.Text = "   Prestasi";
+            this.btn_Prestasi.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Prestasi.UseVisualStyleBackColor = true;
+            // 
+            // btn_Kegiatan
+            // 
+            this.btn_Kegiatan.FlatAppearance.BorderSize = 0;
+            this.btn_Kegiatan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Kegiatan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Kegiatan.Image = ((System.Drawing.Image)(resources.GetObject("btn_Kegiatan.Image")));
+            this.btn_Kegiatan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Kegiatan.Location = new System.Drawing.Point(24, 377);
+            this.btn_Kegiatan.Name = "btn_Kegiatan";
+            this.btn_Kegiatan.Size = new System.Drawing.Size(210, 60);
+            this.btn_Kegiatan.TabIndex = 0;
+            this.btn_Kegiatan.Text = "   Kegiatan";
+            this.btn_Kegiatan.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Kegiatan.UseVisualStyleBackColor = true;
+            // 
+            // btn_Peta
+            // 
+            this.btn_Peta.FlatAppearance.BorderSize = 0;
+            this.btn_Peta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Peta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Peta.Image = ((System.Drawing.Image)(resources.GetObject("btn_Peta.Image")));
+            this.btn_Peta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Peta.Location = new System.Drawing.Point(24, 443);
+            this.btn_Peta.Name = "btn_Peta";
+            this.btn_Peta.Size = new System.Drawing.Size(210, 60);
+            this.btn_Peta.TabIndex = 0;
+            this.btn_Peta.Text = "   Peta";
+            this.btn_Peta.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Peta.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel2.Location = new System.Drawing.Point(3, 179);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 60);
+            this.panel2.TabIndex = 1;
+            // 
+            // AdminAwal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1689, 839);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AdminAwal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdminAwal";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_Tentang;
+        private System.Windows.Forms.Button btn_Slideshow;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_Peta;
+        private System.Windows.Forms.Button btn_Kegiatan;
+        private System.Windows.Forms.Button btn_Prestasi;
+    }
+}
--- GazethruApps/AdminSlideshow.Designer.cs
+++ GazethruApps/AdminSlideshow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GazethruApps
 {
-    partial class AdminInformasi
+    partial class AdminSlideshow
     {
         /// <summary> 
         /// Required designer variable.
@@ -29,75 +29,69 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.textBoxSearch = new System.Windows.Forms.TextBox();
             this.labelSearch = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 79);
+            this.dataGridView1.Location = new System.Drawing.Point(81, 119);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(795, 525);
+            this.dataGridView1.Size = new System.Drawing.Size(540, 570);
             this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(656, 31);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(160, 42);
-            this.btnAdd.TabIndex = 1;
-            this.btnAdd.Text = "Baru";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Location = new System.Drawing.Point(490, 31);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(160, 42);
-            this.btnDelete.TabIndex = 2;
-            this.btnDelete.Text = "Hapus";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            // 
-            // textBoxSearch
-            // 
-            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSearch.Location = new System.Drawing.Point(84, 45);
-            this.textBoxSearch.Name = "textBoxSearch";
-            this.textBoxSearch.Size = new System.Drawing.Size(179, 28);
-            this.textBoxSearch.TabIndex = 3;
-            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
             // 
             // labelSearch
             // 
             this.labelSearch.AutoSize = true;
             this.labelSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSearch.Location = new System.Drawing.Point(25, 45);
+            this.labelSearch.Location = new System.Drawing.Point(84, 71);
             this.labelSearch.Name = "labelSearch";
             this.labelSearch.Size = new System.Drawing.Size(53, 24);
-            this.labelSearch.TabIndex = 4;
+            this.labelSearch.TabIndex = 8;
             this.labelSearch.Text = "Cari :";
             // 
-            // AdminInformasi
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(143, 71);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(179, 28);
+            this.textBoxSearch.TabIndex = 7;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(397, 71);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 42);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Hapus";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(512, 71);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(109, 42);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Baru";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // AdminSlideshow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.labelSearch);
             this.Controls.Add(this.textBoxSearch);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
-            this.Name = "AdminInformasi";
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "AdminSlideshow";
             this.Size = new System.Drawing.Size(1435, 801);
-            this.Load += new System.EventHandler(this.AdminInformasi_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -107,9 +101,9 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.TextBox textBoxSearch;
         private System.Windows.Forms.Label labelSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
