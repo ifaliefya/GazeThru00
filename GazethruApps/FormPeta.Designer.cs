45d01bc68d77fa1104fd450032bb1ba015ab0977
Daryatul Choiriyah <daryatulchoiriyah@gmail.com>
2019-08-07 11:47:33 +0700

    tambah logo teti, teks tombol next peta, nengahin label denah

--- GazethruApps/FormPeta.Designer.cs
+++ GazethruApps/FormPeta.Designer.cs
@@ -59,7 +59,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(569, 2);
+            this.label1.Location = new System.Drawing.Point(860, 2);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(178, 32);
             this.label1.TabIndex = 2;
